2ada6f686b7f0fb9b375cf2b92d2c2cc22cb24f2
sarangholey <sarangholey@gmail.com>
2020-06-08 16:40:49 +0530

    Added some java logic programs

--- src/main/java/JavaLogicalPrograms/_3_ReadIntigerFromUserAndPrint.java
+++ src/main/java/JavaLogicalPrograms/_3_ReadIntigerFromUserAndPrint.java
@@ -1,5 +1,16 @@
 package JavaLogicalPrograms;
 
+import java.util.Scanner;
+
 public class _3_ReadIntigerFromUserAndPrint {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter desired text");
+		
+		String textFromUser = sc.nextLine();
+		
+		System.out.println(textFromUser.toUpperCase());
+	}
+	
 }
